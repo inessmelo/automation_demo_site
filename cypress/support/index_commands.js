0045e9ba49d37c9d88791520e8bdeacd94e47759
Ines Melo <prof.inesmelo@gmail.com>
2025-10-15 22:18:17 -0300

    Adcionando configurações no projeto

--- cypress/support/index_commands.js
+++ cypress/support/index_commands.js
@@ -24,7 +24,6 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-/// <reference types="Cypress" />
 
 Cypress.Commands.add('url', () => {
     cy.visit('https://demo.automationtesting.in/Index.html')
